92eb051290cac852dea6e190ef9a7f2a68428b73
Lappyland-derp <lappyland1102@gmail.com>
2024-04-25 01:11:50 +0800

    commented models
    broken backend still so i commented the models i made

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+# class Creator(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # name = models.CharField(max_length=200, null=True, blank=True)
+    # image =
+    # category = models.CharField(max_length=200, null=True, blank=True)
+    # description = models.TextField(null=True, blank=True)
+    # rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    # numReviews = models.IntegerField(null=True, blank=True, default=0)
+    # price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    # _id = models.AutoField(primary primary_key=True)
+    # createdAt = models.DateTimeField(auto_now_add=True)
 # Create your models here.
