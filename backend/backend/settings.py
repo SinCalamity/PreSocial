653b4c6b7ef6eb58621ec9c91ae729213873b1b7
SinCalamity <vincer.gerente@gmail.com>
2024-04-26 00:40:18 +0800

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,6 +136,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "static cdn", "media root")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+from datetime import timedelta
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
