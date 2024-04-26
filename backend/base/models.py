41b1c715c14273555f4643780a3b012aa5054b10
Lappyland-derp <lappyland1102@gmail.com>
2024-04-26 13:53:39 +0800

    Update models.py

--- backend/base/models.py
+++ backend/base/models.py
@@ -3,23 +3,11 @@ import os
 import random
 
 
-def get_filename_ext(filepath):
-    base_name = os.path.basename(filepath)
-    name, ext = os.path.splitext(base_name)
-    return name, ext
 
-
-
-def upload_image_path(instance, filename):
-    new_filename = random.randint(1, 2541781232)
-    name, ext = get_filename_ext(filename)
-    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
-    return "img/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
-
-    class Creator(models.Model):
+class Creator(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
-    # image =
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
@@ -30,4 +18,16 @@ def upload_image_path(instance, filename):
 
     def _str_(self):
         return self.name
+
+    def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+    def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 2541781232)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "img/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
+
 # Create your models here.
